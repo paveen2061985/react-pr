f877ae1c557b76305969c31288b1f860aab321d1
Praveen Sanikommu <praveenreddy206@gmail.com>
2024-05-23 19:44:28 +0530

    Adding Parcel to Project

--- App.js
+++ App.js
@@ -1,14 +1,5 @@
-// const heading = React.createElement('h1',{id: 'heading'},'Welcome to React');
-// console.log(heading); // It is react element or javascript object
-
-{
-  /* <div id='parent'>
-    <div id='child'>
-        <h1>Welcome to React</h1>
-        <h2>React is Awesome!</h2>
-    </div>
-</div> */
-}
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const parent = React.createElement(
   "div",
@@ -19,7 +10,7 @@ const parent = React.createElement(
       React.createElement("h2", {}, "React is Awesome!"),
     ]),
     React.createElement("div", { id: "child2" }, [
-      React.createElement("h1", {}, "React Library is Powerful"),
+      React.createElement("h1", {}, "React Library is Powerful 🚀"),
       React.createElement("h2", {}, "We can build performant enterprise application using react "),
     ])
   ]
---
+++ README.md
@@ -0,0 +1,18 @@
+# What Parcel does for us
+ - Dev Build
+ - Local Server
+ - HMR(Hot Module Replacement)
+ - Parcel uses File Watching Algorithm, which is written in C++.
+ - Parcel does Caching and hence gives faster builds.
+ - Image Optimization
+ - Minification of files
+ - Bundling
+ - Compress the files
+ - Consistent Hashing.
+ - Differential Bundling => To Support older browsers.
+ - Diagnostics
+ - Better Error Handling.
+ - HTTPS
+ - Tree Shaking - It remove unused code for us.
+ - It create different bundle for Development and Production.
+ - Code Splitting.
